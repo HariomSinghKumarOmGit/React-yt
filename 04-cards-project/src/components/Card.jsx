331c2e5f4172d7d915819e877cee9cee5f7bddd4
hariom singh <hariomsingh@Mac.lan>
2025-10-31 21:20:59 +0530

    test1

--- 04-cards-project/src/components/Card.jsx
+++ 04-cards-project/src/components/Card.jsx
@@ -1,4 +1,4 @@
-/*
+
 import { Bookmark } from 'lucide-react'
 
 export const Card = ({ data }) => {
@@ -28,44 +28,5 @@ export const Card = ({ data }) => {
     </div>
   )
 }
-*/
 
 
-
-
-
-import {Bookmark} from 'lucide-react'
-
-export const Card = (props) => {
-
-    console.log(props.company)
-
-  return (
-    <div className='parent'>
-      <div className="card">
-        <div className="top">
-          <img src="https://i.pinimg.com/1200x/0a/06/60/0a06600cc3cedeb49280b54114c88ce6.jpg" alt="" />
-          <button>Save<Bookmark size={12}/></button>
-        </div>
-        <div className="centre">
-          <h3> {props.company} <span> 5 days ago</span></h3>
-          <h2>Seniour UI/UX Designer</h2>
-          <div className='tag'>
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
-          </div>
-        </div>
-        <div className="bottom">
-          <div>
-            <h1>$120/h</h1>
-            <p>Mumbai, India</p>
-          </div>
-          <div>
-            <button>Apply Now</button>
-          </div>
-        </div>
-      </div>
-      
-    </div>
-  )
-}
