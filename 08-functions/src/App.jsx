76ceb0db26986970b8d2f75b793ef424ebc1deac
hariom singh <hariomsingh@Mac.lan>
2025-11-03 00:32:50 +0530

    worked till hooks

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  const btnClicked = ()=>{
+    console.log('button is clicked')
+  }
+  return (
+
+    <div>
+      <button onClick={btnClicked}>Click</button>
+    </div>
+  )
+}
+
+export default App
