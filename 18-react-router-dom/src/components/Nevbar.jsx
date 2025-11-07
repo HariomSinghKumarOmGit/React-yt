441f8f7e6eac31b6c0a11990104a72ec1d1a5c55
hariom singh <hariomsingh@Mac.lan>
2025-11-07 22:09:07 +0530

    hair i will be changing <a> tag to link get by react

---
+++ 18-react-router-dom/src/components/Nevbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nevbar = () => {
+  return (
+    <div>
+      <div className='nev'>
+        <h2>Thala</h2>
+        <div>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nevbar
